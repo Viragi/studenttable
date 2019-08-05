5f8c4f88f1da88b2ca1f19e2859b2d28462f2d88
shreyansh <viragi311@gmail.com>
2019-08-05 14:05:30 -0700

    cleaning

--- src/reducer/rootreducer.js
+++ src/reducer/rootreducer.js
@@ -4,28 +4,28 @@ let initialState = {
                      {name:"saloni",age:27,dob:"31-Aug-1990"},
                      {name:"Malya",age:32,dob:"17-dec-1987"},
                      {name:"Viragi",age:26,dob:"3-Nov-1993"}],
-    isAsec :true
+    isAsc :true
 };
 
 function rootreducer(state = initialState, action){
-    if(action.type=="sort"){
+    if(action.type==="sort"){
         let reqCol = action.reqCol;
-        let isAsec = state.isAsec;
+        let isAsc = state.isAsc;
         let data = [...state.student_details];
-        if(reqCol =="name"){
+        if(reqCol ==="name"){
             data.sort((a,b)=>{
-                return isAsec? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);     
+                return isAsc? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);     
             })
-        }else if(reqCol =="age"){
+        }else if(reqCol ==="age"){
             data.sort((a,b)=>{
-                return isAsec ? a.age-b.age : b.age-a.age;   
+                return isAsc ? a.age-b.age : b.age-a.age;   
             })
-        }else if(reqCol =="dob"){
+        }else if(reqCol ==="dob"){
             data.sort((a,b)=>{
-                return isAsec ? (new Date(a.dob) - new Date(b.dob)) : (new Date(b.dob) - new Date(a.dob));
+                return isAsc ? (new Date(a.dob) - new Date(b.dob)) : (new Date(b.dob) - new Date(a.dob));
             })
         }
-        return {...state,student_details:data,isAsec:!state.isAsec };
+        return {...state,student_details:data,isAsc:!state.isAsc };
     }else{
         return state;
     }
